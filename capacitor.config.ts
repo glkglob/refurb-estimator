79ab0581c5492af698551e170589962d889b23f7
Kyulg <d.krissolo@icloud.com>
2026-03-26 00:25:24 +0000

    Update webDir in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
-// File: capacitor.config.ts
-import { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
   appId: 'com.rissolo.refurbestimator',
   appName: 'Refurb Estimator',
-  webDir: 'out', // must match Next export folder if you later use static
+  webDir: 'capacitor-www',
   server: {
     url: 'https://refurb-estimator.vercel.app',
     cleartext: true,
   },
 };
-
-export default config;

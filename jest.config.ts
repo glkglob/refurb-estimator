70992b98607a1992de908bb6e0d07d6c23b6243d
Kyulg <sm.krissolo@outlook.com>
2026-04-01 02:46:45 +0100

    chore: remove legacy backend/frontend, finalize Jest setup, cleanup unused files

--- jest.config.ts
+++ jest.config.ts
@@ -4,17 +4,18 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
 
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "^next/server$": "<rootDir>/test/mocks/nextServer.ts"
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!next/)"
+  ],
 
-  transform: {
-    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json" }]
-  }
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"]
 };
 
 export default config;

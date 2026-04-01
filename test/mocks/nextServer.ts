28e3fc38a1e716fb15d4a42d418c583f3295e76a
Kyulg <sm.krissolo@outlook.com>
2026-04-01 02:46:45 +0100

    chore: remove legacy backend/frontend, finalize Jest setup, cleanup unused files

--- test/mocks/nextServer.ts
+++ test/mocks/nextServer.ts
@@ -11,13 +11,14 @@ export class NextRequest extends Request {
 export class NextResponse extends Response {
   static json(data: unknown, init: ResponseInit = {}): NextResponse {
     const headers = new Headers(init.headers);
+
     if (!headers.has("content-type")) {
       headers.set("content-type", "application/json");
     }
 
     return new NextResponse(JSON.stringify(data), {
       ...init,
-      headers
+      headers,
     });
   }
 }
